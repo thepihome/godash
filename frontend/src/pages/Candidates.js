a9a42d2d82f17cb5790dddc9dbda3bfd735e2fe7
Harshad Bhandwaldar <hbhandwaldar@gmail.com>
2025-12-29 16:10:08 -0500

    fixed KPI issue

--- frontend/src/pages/Candidates.js
+++ frontend/src/pages/Candidates.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect, useMemo, useRef } from 'react';
 import { useQuery, useMutation, useQueryClient } from 'react-query';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import api from '../config/api';
